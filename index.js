ce91bc500e620c6430342909f90ca8355217f3b6
Sulagna Shuin <109204361+sulagnashuin99@users.noreply.github.com>
2022-09-14 11:40:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('input');
+let buttons = Array.from(document.getElementsByClassName('btn-btn'));
+buttons.map(button => {
+  button.addEventListener('click', (e) => {
+    switch (e.target.innerText) {
+      case 'Clear':
+        display.innerText = '';
+        break;
+      case '←':
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case '=':
+        try{
+            display.innerText = eval(display.innerText);
+        } catch {
+            display.innerText = "Error!"
+        }
+        break; 
+      default:
+        display.innerText += e.target.innerText;
+       }
+  })
+})
